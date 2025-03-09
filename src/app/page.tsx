06915fbc9600bff39b55f7f05ca6e0125117e816
ykwimk <ykwimk@gmail.com>
2025-03-09 10:48:29 +0900

    feat: 메인 화면에서 쿠키값 있을 경우 dashboard 리다이렉트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,16 @@
+import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 import HomeContainer from '@/components/home/HomeContainer';
 
-export default function Home() {
+export default async function Home() {
+  const cookieStore = await cookies();
+
+  const accessToken = cookieStore.get(process.env.SERVICE_ACCESS_TOKEN!)?.value;
+  const userId = cookieStore.get(process.env.SERVICE_USER_ID!)?.value;
+
+  if (accessToken && userId) {
+    return redirect('/dashboard');
+  }
+
   return <HomeContainer />;
 }
